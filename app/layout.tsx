c3f9ea9050652d4cbbe8dfc3d43970d43a07f876
Joshua Hill <36944169+joshh9305@users.noreply.github.com>
2026-04-02 13:56:57 -0400

    add a header and footer to home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-slate-50 min-h-screen">{children}</body>
+      
+      <body className="bg-slate-50 min-h-screen">
+        <h1>Pokémon Plushie Collection</h1>
+        {children}
+        <footer>
+          <p>&copy; 2023 Pokémon Plushie Collection. All rights reserved.</p>
+        </footer>
+      </body>
     </html>
   );
 }
